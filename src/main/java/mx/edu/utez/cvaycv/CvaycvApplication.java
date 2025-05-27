be33b49b2c6a107632b38343c17925017083d39d
AnettVera <20223tn034@utez.edu.mx>
2025-05-27 15:50:42 -0600

    [FIRST COMMIT]Subida inicial del proyecto

---
+++ src/main/java/mx/edu/utez/cvaycv/CvaycvApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvaycv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvaycvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvaycvApplication.class, args);
+	}
+
+}
